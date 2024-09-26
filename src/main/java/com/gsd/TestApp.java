ad086516f1d729a91434fd18895fe4379e30cee6
Ali Azfar <aazfar1@depaul.edu>
2024-09-25 19:57:00 -0500

    Modified TestApp complete ToDo

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -6,12 +6,7 @@ public class TestApp {
     public static void main(String[] args){
         System.out.println("SE475/371 GSD Course \n");
 
-
-        System.out.println("Name: Dave Shilander");
-
-        //TODO Print your name like this "Name: Vahid Alizadeh"
-        System.out.println("Name: Muhammad Sameel Shahid");
-
+        System.out.println("Name: Ali Azfar");
 
     }
 }
