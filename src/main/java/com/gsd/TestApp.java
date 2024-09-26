b2b4e0dab9136c230f04e6921b57835dcf81504d
Janiya G <95838190+JEG2004@users.noreply.github.com>
2024-09-25 20:56:18 -0500

    Done (#48)

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -5,6 +5,7 @@ public class TestApp {
 
     public static void main(String[] args){
         System.out.println("SE475/371 GSD Course \n");
+        System.out.println("Janiya Garner");
 
 
         //TODO Print your name like this "Name: Vahid Alizadeh"
