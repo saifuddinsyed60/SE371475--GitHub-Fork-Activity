3290377e65644dae2cf081e5bff110fb0f41e745
Dave Leo Shilander <dave.leo.shilander@icloud.com>
2024-09-25 19:44:59 -0500

    Added print statement for name in TestApp

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -6,7 +6,6 @@ public class TestApp {
     public static void main(String[] args){
         System.out.println("SE475/371 GSD Course \n");
 
-        //TODO Print your name like this "Name: Vahid Alizadeh"
-
+        System.out.println("Dave Shilander");
     }
 }
